72a27d55c5dccb4799cacb5205802a720b94cdb8
JoshTrev <joshj438t@gmail.com>
2020-10-08 15:20:48 -0400

    working on server.js and handlebars files in view folder

--- server.js
+++ server.js
@@ -1,35 +1,47 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 3000;
+var exphbs = require("express-handlebars");
 
 var app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 3000;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+const burgers = [
+  {
+    id: 1,
+    name: "Bacon Burger",
+    devoured: false
+  },
+  {
+    id: 2,
+    name: "Cheese Burger",
+    devoured: false
+  },
+  {
+    id: 3,
+    name: "Double Cheese Burger",
+    devoured: true
+  },
+  {
+    id: 4,
+    name: "Veggie Burger",
+    devoured: true
+  }
+];
 
-app.use(routes);
+app.get("/", (req, res) => {
+    res.render("index", {
+      burgers: burgers
+    });
+});
+
+app.put("/api/burgers/:id", (req, res) => {
+    const chosen = req.params.id;
+
+    // MySQL can update burger devoured false to devoured true where id matches chosen
+})
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
-
-
-
-
-
-// app.get("/", (req, res) => {
-//     res.render("index");
-// });
