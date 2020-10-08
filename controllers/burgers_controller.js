f593c35845d856dc59ebb8b30b637b8b81981061
JoshTrev <joshj438t@gmail.com>
2020-10-08 19:30:56 -0400

    Got adding burger and devouring burger working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,80 +5,44 @@ var router = express.Router();
 // Import the model to use its database functions.
 var burgerModel = require("../models/burgerModel.js");
 
-// const burgers = [
-//     {
-//       id: 1,
-//       name: "Bacon Burger",
-//       devoured: false
-//     },
-//     {
-//       id: 2,
-//       name: "Cheese Burger",
-//       devoured: false
-//     },
-//     {
-//       id: 3,
-//       name: "Double Cheese Burger",
-//       devoured: true
-//     },
-//     {
-//       id: 4,
-//       name: "Veggie Burger",
-//       devoured: true
-//     }
-//   ];
-
-
-
-
 router.get("/", (req, res) => {
-    burgerModel.selectAll(function(data) {
+    burgerModel.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
         console.log("hbsObject from burgers_controller.js");
         console.log(hbsObject);
         res.render("index", hbsObject);
-      });
+    });
 });
 
-
-  
-
-//   router.get("/", (req, res) => {
-//     res.render("index", {
-//       burgers: burgers
-//     });
-// });
-
-
-
-
-
-
-
-
-
-router.get("/api/burgers", (req, res) => {
-// MySQL Select retrieves burgers array from DB
-  res.json(burgers);
+router.post("/api/burgers", function (req, res) {
+    burgerModel.create("burger_name", [req.body.burger_name], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
 });
 
-router.post("/api/burgers", (req, res) => {
-  let newBurger = req.body;
-  // MySQL Insert
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burgerModel.update(
+        {
+            devoured: 1
+        },
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            res.status(200).end();
+
+        }
+    );
 });
-
-router.put("/api/burgers/:id", (req, res) => {
-  const chosen = req.params.id;
-
-  // MySQL Update can update burger devoured false to devoured true where id matches chosen
-});
-
-
-
-
-
 
 // Export routes for server.js to use.
 module.exports = router;
