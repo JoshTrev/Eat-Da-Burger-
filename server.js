0f91b79f8ac063e51a19d56f610e9be7c1dede43
JoshTrev <joshj438t@gmail.com>
2020-10-08 20:22:35 -0400

    Linked CSS file, need to complete css file.

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
 
-var app = express();
 var PORT = process.env.PORT || 3000;
 
-// app.use(express.static("public"));
+var app = express();
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
